46d7e406aaf71b4e21ca0a42ba5eceeeb948dbcb
seungjin <seungjin429@naver.com>
2019-10-10 20:04:13 +0900

    add retry viewholder

---
+++ app/src/main/java/kr/co/alex/weathersample/adapter/WeatherRetryViewHolder.kt
@@ -0,0 +1,10 @@
+package kr.co.alex.weathersample.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.viewholder_retry.*
+import kotlinx.android.synthetic.main.viewholder_retry.view.*
+
+class WeatherRetryViewHolder(override val containerView: View) :
+    RecyclerView.ViewHolder(containerView), LayoutContainer
